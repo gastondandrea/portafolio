1e87d4808a60750f095f370f856226a6eec99b84
gastondandrea <gastondandrea@hotmail.com>
2025-03-24 15:44:46 -0300

    nav-bar

--- Client/src/components/navBar/navBar.jsx
+++ Client/src/components/navBar/navBar.jsx
@@ -1,16 +1,23 @@
+import './navBar.css'
+import linkedin from "../../assets/img/linkedin.svg"
+import github from "../../assets/img/github.svg"
+
 function NavBar() {
     return (
-      <nav style={{ background: '#FDFFFC', padding: '10px', color: 'white' }}>
-        <ul style={{ display: 'flex', gap: '15px', listStyle: 'none', margin: 0, padding: 0 }}>
-          <li><a href="/" style={{ color: '#011627', textDecoration: 'none' }}>Gaston D'Andrea</a></li>
-          <li><a href="/" style={{ color: '#011627', textDecoration: 'none' }}>Inicio</a></li>
-          <li><a href="#acerca-de-mi" style={{ color: '#011627', textDecoration: 'none' }}>Acerca de mi</a></li>
-          <li><a href="#portafolio" style={{ color: '#011627', textDecoration: 'none' }}>Portafolio</a></li>
-          <li><a href="/contacto" style={{ color: '#011627', textDecoration: 'none' }}>Contacto</a></li>
+      <nav className='container-nav-nav'>
+        <ul className='container-ul-nav'>
+          <li className='contaner-name-nav'><a href="/"><strong>Gaston</strong> D'Andrea</a></li>
+          <div className='container-hamburger-menu-nav'>
+            <li><a href="/">Inicio</a></li>
+            <li><a href="#acerca-de-mi">Acerca de mi</a></li>
+            <li><a href="#portafolio">Portafolio</a></li>
+            <li className='contaner-logo-nav'><img src={linkedin} alt="linkedin" /></li>
+            <li className='contaner-logo-nav'><img src={github} alt="github" /></li>
+            <li className='contaner-contact-nav'><a href="/contacto">Contacto</a></li>
+          </div>
         </ul>
       </nav>
     );
   }
-  
 
 export default NavBar;
