bb95a79fe1cd155e0f9ed6a7328cb05e53801704
gastondandrea <gastondandrea@hotmail.com>
2025-04-10 20:22:18 -0300

    navbar 10/4 20:22

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,14 +3,12 @@ import './App.css'
 import {Routes, Route} from "react-router-dom";
 
 import HomePage from './views/HomePage/HomePage';
-import ContactoPage from './views/ContactoPage/ContactoPage';
 
 function App() {
   return (
     <>
     <Routes>
         <Route path="/" element={<HomePage/>} />
-        <Route path="/contacto" element={<ContactoPage/>} />
       </Routes>
     </>
   )
--- Client/src/components/navBar/navBar.jsx
+++ Client/src/components/navBar/navBar.jsx
@@ -6,38 +6,50 @@ import hamburgerMenu from "../../assets/img/hamburger-menu.svg";
 import closeMenu from "../../assets/img/close-menu.svg";
 
 function NavBar() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-      setIsMenuOpen(prev => !prev);
-    };
-
-    return (
-      <nav className='container-nav-nav'>
-        <ul className='container-ul-nav'>
-          <li className='contaner-name-nav'>
-            <a href="/"><strong>Gaston</strong> D'Andrea</a>
-          </li>
-          {/* Botón hamburguesa */}
-          <div className="container-img-hamburgerMenu" onClick={toggleMenu}>
-            <img
-              src={isMenuOpen ? closeMenu : hamburgerMenu}
-              alt={isMenuOpen ? "closeMenu" : "hamburgerMenu"}
-            />
-          </div>
-          {/* Menú navegable */}
-          <div className={`container-hamburger-menu-nav ${isMenuOpen ? 'show' : ''}`}>
-            <li><a href="/">Inicio</a></li>
-            <li><a href="/">Acerca de mi</a></li>
-            <li><a href="/">Portafolio</a></li>
-            <li className='contaner-logo-nav'><a href="https://linkedin.com/in/gastondandrea"><img src={linkedin} alt="linkedin" /></a></li>
-            <li className='contaner-logo-nav'><a href="https://github.com/gastondandrea"><img src={github} alt="github" /></a></li>
-            <li className='contaner-contact-nav'><a href="/contacto">Contacto</a></li>
-          </div>
-        </ul>
-         
-      </nav>
-    );
+  const toggleMenu = () => {
+    setIsMenuOpen(prev => !prev);
   };
+
+  const handleLinkClick = () => {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  return (
+    <nav className='navbar-container'>
+      <ul className='navbar-content'>
+        <li className='navbar-brand'>
+          <a href="/" onClick={handleLinkClick}><strong>Gaston</strong> D'Andrea</a>
+        </li>
+
+        <div className="navbar-toggle" onClick={toggleMenu}>
+          <img
+            src={isMenuOpen ? closeMenu : hamburgerMenu}
+            alt={isMenuOpen ? "close menu" : "open menu"}
+          />
+        </div>
+
+        <div className={`navbar-links ${isMenuOpen ? 'show' : ''}`}>
+          <li><a href="#Acerca de mi" onClick={handleLinkClick}>Acerca de mi</a></li>
+          <li><a href="#Portafolio" onClick={handleLinkClick}>Portafolio</a></li>
+          <li className='navbar-icon'>
+            <a href="https://linkedin.com/in/gastondandrea" target="_blank" rel="noopener noreferrer" onClick={handleLinkClick}>
+              <img src={linkedin} alt="linkedin" />
+            </a>
+          </li>
+          <li className='navbar-icon'>
+            <a href="https://github.com/gastondandrea" target="_blank" rel="noopener noreferrer" onClick={handleLinkClick}>
+              <img src={github} alt="github" />
+            </a>
+          </li>
+          <li className='navbar-contact'><a href="#contacto" onClick={handleLinkClick}>Contacto</a></li>
+        </div>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
